30c92f29848c52bbc28304ba23343894f806b27e
Soni Bro <shantanusoni979@gmail.com>
2025-01-19 22:09:00 +0530

    Done development

--- src/components/forms/StudentForm.tsx
+++ src/components/forms/StudentForm.tsx
@@ -3,7 +3,7 @@
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import InputField from "../InputField";
+import InputField from "@/components/InputField";
 import Image from "next/image";
 
 const schema = z.object({
